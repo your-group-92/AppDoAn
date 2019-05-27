c025f645956a469a2e8860a4b4158af5109afbe8
Pandora <chenhuanhui123@gmail.com>
2019-05-27 23:07:58 +0700

    AppGiaiTri

--- app/src/main/java/com/example/appgiaitri/MainActivity.java
+++ app/src/main/java/com/example/appgiaitri/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnmusic, btnaudio;
+    Button btnmusic, btnaudio, btnabout, btnexit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +16,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnmusic = (Button) findViewById(R.id.btnmusic);
-
         btnmusic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent pagemusic = new Intent(MainActivity.this,Music.class);
 
                 startActivity(pagemusic);
+            }
+        });
+
+        btnaudio = (Button) findViewById(R.id.btnaudio);
+        btnaudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent listaudio = new Intent(MainActivity.this,list_audio.class);
+                startActivity(listaudio);
+            }
+        });
+
+        btnexit = (Button)findViewById(R.id.btnexit);
+        btnexit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
             }
         });
     }

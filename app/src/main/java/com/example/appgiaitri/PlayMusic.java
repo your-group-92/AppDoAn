6754197f4102a7efd1ddbd9f53f82a7fd4862bbe
= <=>
2019-05-27 13:59:06 +0700

    change 14:00

--- app/src/main/java/com/example/appgiaitri/PlayMusic.java
+++ app/src/main/java/com/example/appgiaitri/PlayMusic.java
@@ -3,15 +3,20 @@ package com.example.appgiaitri;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class PlayMusic extends AppCompatActivity {
@@ -27,6 +32,8 @@ public class PlayMusic extends AppCompatActivity {
     ImageView imgdisc;
 
     ImageButton imgbtnPrev, imgbtnPlay, imgbtnStop, imgbtnNext;
+
+    Animation animation;
 
     int position = 0 ;
 
@@ -49,11 +56,112 @@ public class PlayMusic extends AppCompatActivity {
 
         position = b.getInt("pos",0);
 
+        animation = AnimationUtils.loadAnimation(this,R.anim.disc_rotate);
+
         AnhXa();
 
         KhoiTao();
 
+        CapNhatTimeSong();
+
         mediaPlayer.start();
+
+        imgdisc.startAnimation(animation);
+
+        imgbtnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()==true){
+                    //nếu đang phát -> pause -> đổi hình play
+                    mediaPlayer.pause();
+
+                    imgbtnPlay.setImageResource(R.drawable.play64);
+                    imgdisc.clearAnimation();
+                }else {
+                    //đang ngừng -> phát -> đổi hình pause
+                    mediaPlayer.start();
+                    imgbtnPlay.setImageResource(R.drawable.pause64);
+                    imgdisc.startAnimation(animation);
+                }
+                SetTimeEnd();
+            }
+        });
+
+        imgbtnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mediaPlayer.stop();
+
+                mediaPlayer.release();
+
+                imgbtnPlay.setImageResource(R.drawable.play64);
+
+                KhoiTao();
+
+                imgdisc.clearAnimation();
+            }
+        });
+
+        imgbtnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                position++;
+                if(position > mySongs.size() -1){
+                    position = 0;
+                }
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                }
+
+                KhoiTao();
+
+                imgdisc.startAnimation(animation);
+
+                mediaPlayer.start();
+
+                imgbtnPlay.setImageResource(R.drawable.pause64);
+            }
+        });
+
+        imgbtnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                position--;
+                if (position < 0) {
+                    position = mySongs.size() - 1;
+                }
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                }
+
+                KhoiTao();
+
+                imgdisc.startAnimation(animation);
+
+                mediaPlayer.start();
+                imgbtnPlay.setImageResource(R.drawable.pause64);
+            }
+        });
+
+        skbarSong.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(skbarSong.getProgress());
+            }
+        });
 
     }
 
@@ -76,13 +184,77 @@ public class PlayMusic extends AppCompatActivity {
 
         Uri u =Uri.parse(mySongs.get(position).toString());
 
-
         mediaPlayer =MediaPlayer.create(getApplicationContext(),u);
 
         txtTitle.setText(mySongs.get(position).getName().replace(".mp3","").replace(".flac","").toString());
 
+        SetTimeEnd();
+    }
 
-        //SetTimeEnd();
+    private  void SetTimeEnd(){
+        SimpleDateFormat dinhdanggio = new SimpleDateFormat("mm:ss");
+
+        txtTimeEnd.setText(dinhdanggio.format(mediaPlayer.getDuration()));
+
+        //gán max của skbarSong = mediaPlayer.getDuratiom()
+
+        skbarSong.setMax(mediaPlayer.getDuration());
+    }
+
+    private  void CapNhatTimeSong() {
+        final Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                SimpleDateFormat dinhdanggio = new SimpleDateFormat("mm:ss");
+
+                txtTimeStart.setText(dinhdanggio.format(mediaPlayer.getCurrentPosition()));
+
+                //cập nhật progress skbarSong
+
+                skbarSong.setProgress(mediaPlayer.getCurrentPosition());
+
+                //kiểm tra thời gian bài hát -> nếu kết thúc -> next
+
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+
+                        position++;
+
+                        if (position > mySongs.size() - 1) {
+
+                            position = 0;
+
+                        }
+
+                        if (mediaPlayer.isPlaying()) {
+
+                            mediaPlayer.stop();
+
+                        }
+
+                        KhoiTao();
+
+                        txtTitle.setText(mySongs.get(position).getName().replace(".mp3","").replace(".flac","").toString());
+
+                        mediaPlayer.start();
+
+                        imgbtnPlay.setImageResource(R.drawable.pause64);
+
+                        SetTimeEnd();
+
+                        CapNhatTimeSong();
+
+                    }
+                });
+
+                handler.postDelayed(this, 500);
+
+            }
+        }, 100);
     }
 
 }

a77cecb5ade0de2568fe5040a17558699c3c48b9
= <=>
2019-05-28 10:57:05 +0700

    change 11:00

--- app/src/main/java/com/example/appgiaitri/MainActivity.java
+++ app/src/main/java/com/example/appgiaitri/MainActivity.java
@@ -1,10 +1,26 @@
 package com.example.appgiaitri;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +35,8 @@ public class MainActivity extends AppCompatActivity {
         btnmusic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent pagemusic = new Intent(MainActivity.this,Music.class);
+                requestStoragePermission();
 
-                startActivity(pagemusic);
             }
         });
 
@@ -31,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent listaudio = new Intent(MainActivity.this,list_audio.class);
                 startActivity(listaudio);
+
             }
         });
 
@@ -41,5 +57,70 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });
+    }
+
+    public void requestStoragePermission(){
+
+        Dexter.withActivity(MainActivity.this)
+                .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if(report.areAllPermissionsGranted()){
+                            Intent pagemusic = new Intent(MainActivity.this,Music.class);
+
+                            startActivity(pagemusic);
+                        }
+
+                        if(report.isAnyPermissionPermanentlyDenied()){
+                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                })
+                .withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(MainActivity.this,"Error Occured While Granting Permission",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
+
+    public void showSettingsDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("Need Permission");
+
+        builder.setMessage("This app needs permission to use this feature . You can grant this from app setting");
+
+        builder.setPositiveButton("Go To Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                openSettings();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    public void openSettings(){
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package",getPackageName(),null);
+        intent.setData(uri);
+        startActivity(intent);
     }
 }
